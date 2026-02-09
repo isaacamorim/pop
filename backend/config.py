e529ece8f063d2921a980f09fd905a1717222c22
isaacamorim <126696656+isaacamorim@users.noreply.github.com>
2026-02-09 09:44:15 -0300

    Change default DB config values to empty strings

--- backend/config.py
+++ backend/config.py
@@ -5,10 +5,10 @@ from urllib.parse import quote_plus
 
 
 class Config:
-    DB_USER = os.getenv("DB_USER", "SYSALL")
-    DB_PASS = os.getenv("DB_PASS", "LEGEND")
-    DB_HOST = os.getenv("DB_HOST", "10.42.92.200")
-    DB_PORT = os.getenv("DB_PORT", "1521")
+    DB_USER = os.getenv("DB_USER", "")
+    DB_PASS = os.getenv("DB_PASS", "")
+    DB_HOST = os.getenv("DB_HOST", "")
+    DB_PORT = os.getenv("DB_PORT", "")
     DB_SERVICE = os.getenv("DB_SERVICE", "ORCL")
 
     EMPRESA_PADRAO = os.getenv("EMPRESA_PADRAO", "1")
